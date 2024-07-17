f272addb73685f41694abe7d2c3f7e9bb02f219a
candy-crimew <calansk0@gmail.com>
2024-07-16 18:43:43 -0700

    Added dependencies and Hexxed effect

---
+++ src/main/java/io/github/candycalc/phlexiful/effect/HexxedEffect.java
@@ -0,0 +1,10 @@
+package io.github.candycalc.phlexiful.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+
+public class HexxedEffect extends StatusEffect {
+    protected HexxedEffect(StatusEffectType statusEffectType, int color) {
+        super(statusEffectType, color);
+    }
+}
---
+++ src/main/java/io/github/candycalc/phlexiful/effect/ModEffects.java
@@ -0,0 +1,20 @@
+package io.github.candycalc.phlexiful.effect;
+
+import io.github.candycalc.phlexiful.Phlexiful;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModEffects extends StatusEffects {
+    public static StatusEffect HEXXED;
+
+    private static StatusEffect register(String name, StatusEffect entry) {
+        return Registry.register(Registry.STATUS_EFFECT, new Identifier(Phlexiful.MOD_ID, name), entry);
+    }
+
+    public static void registerEffect() {
+        HEXXED = register("hexxed", (new HexxedEffect(StatusEffectType.HARMFUL, 13524717)));
+    }
+}
