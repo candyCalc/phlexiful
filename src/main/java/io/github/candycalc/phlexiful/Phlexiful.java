1a12708da3b5dba104e301a0957dc330fa43b44c
candy-crimew <calansk0@gmail.com>
2024-07-16 18:38:51 -0700

    Added dependencies and Hexxed effect

--- src/main/java/io/github/candycalc/phlexiful/Phlexiful.java
+++ src/main/java/io/github/candycalc/phlexiful/Phlexiful.java
@@ -5,11 +5,16 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.candycalc.phlexiful.effect.ModEffects;
+
 public class Phlexiful implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("Phlexiful");
+    public static final String MOD_ID = "phlexiful";
+    public static final Logger LOGGER = LoggerFactory.getLogger("phlexiful");
 
     @Override
     public void onInitialize(ModContainer mod) {
         LOGGER.info("Hello Quilt world from {}! Stay fresh!", mod.metadata().name());
+
+        ModEffects.registerEffect();
     }
 }
