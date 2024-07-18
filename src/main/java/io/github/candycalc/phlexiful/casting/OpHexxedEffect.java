f5c8a3a4fec0505cda4409e6b6d68421c476f651
candy-crimew <calansk0@gmail.com>
2024-07-18 05:01:11 -0700

    Pattern for hexxed effect

--- src/main/java/io/github/candycalc/phlexiful/casting/OpHexxedEffect.java
+++ src/main/java/io/github/candycalc/phlexiful/casting/OpHexxedEffect.java
@@ -1,30 +1,31 @@
 package io.github.candycalc.phlexiful.casting;
 
+import io.github.candycalc.phlexiful.effect.ModEffects;
+
 import at.petrak.hexcasting.api.misc.MediaConstants;
-import at.petrak.hexcasting.api.spell.OperationResult;
-import at.petrak.hexcasting.api.spell.ParticleSpray;
-import at.petrak.hexcasting.api.spell.OperatorUtils;
-import at.petrak.hexcasting.api.spell.RenderedSpell;
-import at.petrak.hexcasting.api.spell.SpellAction;
+import at.petrak.hexcasting.api.spell.*;
 import at.petrak.hexcasting.api.spell.casting.CastingContext;
 import at.petrak.hexcasting.api.spell.casting.eval.SpellContinuation;
 import at.petrak.hexcasting.api.spell.iota.Iota;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.text.Text;
+
 import kotlin.Triple;
 import java.util.List;
 
-import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
 public class OpHexxedEffect implements SpellAction {
 	public OpHexxedEffect() {}
 
 	@Override
-	public int getArgc() {return 1;}
+	public int getArgc() {return 4;}
 	@Override
-	public boolean hasCastingSound(@NotNull CastingContext ctx) {return true;}
+	public boolean hasCastingSound(@NotNull CastingContext context) {return true;}
 	@Override
-	public boolean awardsCastingStat(@NotNull CastingContext ctx) {return true;}
+	public boolean awardsCastingStat(@NotNull CastingContext context) {return true;}
 	@Override
 	public boolean isGreat() {return false;}
 	@Override
@@ -36,19 +37,43 @@ public class OpHexxedEffect implements SpellAction {
 
 	@Override
 	public Triple<RenderedSpell, Integer, List<ParticleSpray>> execute(@NotNull List<? extends Iota> args, @NotNull CastingContext context) {
+		// Pattern arguments
+		LivingEntity target = OperatorUtils.getLivingEntityButNotArmorStand(args, 0, getArgc());
+		List<SpellList> spell = List.of(OperatorUtils.getList(args, 1, getArgc()));
+		Double duration = OperatorUtils.getPositiveDouble(args, 2, getArgc());
+		Double battery = OperatorUtils.getPositiveDouble(args, 3, getArgc());
+
+		//check if target is in bounds
+		context.assertEntityInRange(target);
+
+		//calculate cost
+		int cost = MediaConstants.DUST_UNIT * (battery.intValue() + 10);
 
 		return new Triple<RenderedSpell, Integer, List<ParticleSpray>>(
-			new Spell(),
-			MediaConstants.DUST_UNIT,
-			List.of()
+			new Spell(target, spell, duration, battery),
+			cost,
+			List.of(ParticleSpray.cloud(target.getPos(), 1.0, 50))
 		);
 	}
 
-	private class Spell implements RenderedSpell {
-		public Spell() {}
+	public class Spell implements RenderedSpell {
+		private LivingEntity target;
+		private List<SpellList> spell;
+		private Double duration;
+		private Double battery;
+
+		public Spell(LivingEntity target, List<SpellList> spell, Double duration, Double battery) {
+			this.target = target;
+			this.spell = spell;
+			this.duration = duration;
+			this.battery = battery;
+		}
 
 		@Override
-		public void cast(@NotNull CastingContext ctx) {}
+		public void cast(@NotNull CastingContext context) {
+			StatusEffectInstance effectInstance = new StatusEffectInstance(ModEffects.HEXXED, duration.intValue() * 20, 0);
+			target.addStatusEffect(effectInstance);
+		}
 	}
 
 	@Override @NotNull
